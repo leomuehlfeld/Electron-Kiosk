514271ea5fae8b884bad0c3e1c1fe7d0ce668724
Leo Mühlfeld <leo.muehlfeld@icloud.com>
2019-09-14 22:38:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Electron-Kiosk
+ Simple Electron input app – originally made for Raspberrypi.
---
+++ src/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    kiosk: true,
+    width: 1024,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('src/index.html')
+  // win.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
